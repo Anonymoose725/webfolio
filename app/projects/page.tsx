dabd80736b29606b1ce35a9b7a3cd77ff03b68e5
Anonymoose725 <ethangat725@gmail.com>
2026-08-24 16:31:13 -0400

    projects layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,43 @@
-// app/about/page.tsx
+import { ProjectEntry } from "@/app/ui/project-entry";
+import { DimensionDivider } from "@/app/ui/dimension-divider";
+
+// temp projects
+const projects = [
+    {
+        title: "Webfolio",
+        blurb: "This site: a Next.js framework portfolio to showcase my projects and thoughts in logs. Created to learn how to use external libraries and devtools.",
+        tags: ["Next.js", "TailwindCSS", "Supabase", "Markdown"],
+        href: "https://www.google.com",
+    },
+    {
+        title: "Haskell Algorithm Visualizer",
+        blurb: "An algorithm visualizer with a backend written in Haskell. Designed to demonstrate classic CS algorithms in a manner that is easy to understand if you can't grasp the concept by looking at code alone.",
+        tags: ["Haskell", "Servant", "React", "DSA"],
+        href: "https://github.com/Anonymoose725/algo-visualizer",
+    },
+    {
+        title: "Webfo2lio",
+        blurb: "It's a website AND a portfolio. How is this possible??",
+        tags: ["test", "test"],
+        href: "google.com",
+    },
+];
+
 export default function ProjectsPage() {
+
     return (
-        <main className="p-8">
-            <h1 className="text-2xl font-bold">Projects</h1>
-            <p>Placeholder content.</p>
-        </main>
+        <div className="mx-auto max-w-2xl px-8 py-16">
+            <DimensionDivider label="FIG. 03 - PROJECTS" />
+            <h1 className="mt-6 mb-10 font-heading text-5xl font-bold tracking-wide">
+                PROJECTS
+            </h1>
+
+            <div className="space-y-4"> {/* adds vertical space inside a parent container between children */}
+                {projects.map((p, i) => (
+                    // key is a react bookkeeper index for the map function! not a prop of ProjectEntry.
+                    <ProjectEntry key={p.title} project={p} index={i} />
+                ))}
+            </div>
+        </div>
     );
 }
