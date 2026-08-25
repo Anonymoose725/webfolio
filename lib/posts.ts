ed235114370537dddb9d6b3f2167982c12580de5
Anonymoose725 <ethangat725@gmail.com>
2026-08-25 18:14:45 -0400

    library and utility dir, blogpost slug functions

---
+++ lib/posts.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';                // node promise-based filesystem (async, await)
+import path from 'path';            // transforming file and directory path
+import matter from 'gray-matter';   // fontmatter parser
+
+const postsDir = path.join(process.cwd(), 'content/logs'); // cwd = current working directory
+
+export type PostMeta = {
+    slug: string,
+    title: string,
+    date: string,
+    summary: string
+};
+
+export function getAllPosts(): PostMeta[] {
+    const files = fs.readdirSync(postsDir).filter((f) => f.endsWith('.mdx')); // read contents of directory, only read .mdx
+
+    const posts = files.map((file) => {
+        const slug = file.replace(/\.mdx$/, '');                        // remove file extension to generate slug
+        const src = fs.readFileSync(path.join(postsDir, file), 'utf8'); // read files and encode in UTF-8
+        const { data } = matter(src);                                   // extract and parse fontmatter object
+        return {
+            slug,
+            title: data.title as string, // type strict
+            date: data.date as string,
+            summary: data.summary as string
+        };
+    });
+
+    return posts.sort((a, b) => (a.date < b.date ? 1 : -1)); // return posts as array of slugs and metadata
+}
+
+export function getPostSource(slug: string): string { // retrieve source from slug
+    const filePath = path.join(postsDir, `${slug}.mdx`);
+    return fs.readFileSync(filePath, 'utf8');
+}
+
+
+// getAllPosts only reads fontmatter, does not render markdown pages
+// getPostSource retrieves the source, so only that slug is rendered on demand
