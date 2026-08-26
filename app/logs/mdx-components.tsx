7cd588f57f0b22d54cd7cd9efd53a1a581225b29
Anonymoose725 <ethangat725@gmail.com>
2026-08-25 23:06:20 -0400

    classname typo

--- app/logs/mdx-components.tsx
+++ app/logs/mdx-components.tsx
@@ -4,7 +4,7 @@ export const mdxComponents = {
     // custom component map for export!
     // without this, mdx wouldnt match the site's design, in an unstyled browser default for markdowns
     h2: (props: any) => (
-        <h2 className="mt-10 mb-4 font-heading text-2xl fond-bold tracking-wide" {...props} /> // note ... forwards all props over
+        <h2 className="mt-10 mb-4 font-heading text-2xl font-bold tracking-wide" {...props} /> // note ... forwards all props over
     ),
     p: (props: any) => (
         <p className="mb-4 text-blueprint-line" {...props} />
