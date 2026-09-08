40a07ae13fb2d2771194ce26389cee12abb45082
Ethan <ethan.gat@mail.mcgill.ca>
2026-09-07 23:36:03 -0400

    metadata update to reflect new URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://ethangat.com/"), // vercel URL
+    metadataBase: new URL("https://ethangat.com/"),
     title: "Ethan Gat Webfolio",
     description: "A web portfolio demonstrating Ethan Gat's projects and blogs",
     openGraph: {
